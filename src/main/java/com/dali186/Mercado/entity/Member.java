3a2cf6056598df7896517069aea55be607b11ddf
jwkim <wer07032@naver.com>
2025-01-07 11:15:47 +0900

    2-11. (공통) 회원 등록 - #3
    Entity 클래스에 기본 생성자 어노테이션 추가
    
    Hibernate는 엔티티 클래스를 생성할 때 기본 생성자를 사용

--- src/main/java/com/dali186/Mercado/entity/Member.java
+++ src/main/java/com/dali186/Mercado/entity/Member.java
@@ -10,11 +10,13 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Member extends BaseEntity {
 	
--- src/main/java/com/dali186/Mercado/entity/Post.java
+++ src/main/java/com/dali186/Mercado/entity/Post.java
@@ -8,11 +8,13 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Post extends BaseEntity {
 	
